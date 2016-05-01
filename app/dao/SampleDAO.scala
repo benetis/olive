f86970b9d222ae7095c4c68233097580172b0192
zygimantas <zygimantasdev@gmail.com>
2016-05-01 22:14:30 +0300

    return all samples from DB

--- app/dao/SampleDAO.scala
+++ app/dao/SampleDAO.scala
@@ -6,11 +6,14 @@ import models.Sample
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
 import slick.lifted.ProvenShape
+import scala.concurrent.Future
 
 class SampleDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
   import driver.api._
 
-  private val samples = TableQuery[SamplesTable]
+  private val Samples = TableQuery[SamplesTable]
+
+  def all(): Future[Seq[Sample]] = db.run(Samples.result)
 
   private class SamplesTable(tag: Tag) extends Table[Sample](tag, "SAMPLE") {
     def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
