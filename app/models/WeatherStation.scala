f9b2748a73e86925880af15914d01a5643b6aeb7
zygimantas <zygimantasdev@gmail.com>
2016-05-06 20:25:59 +0300

    added weather station model

---
+++ app/models/WeatherStation.scala
@@ -0,0 +1,15 @@
+package models
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.UUID
+
+import play.api.libs.json.{Json, _}
+
+case class WeatherStation (id: Option[Long],
+                           key: UUID,
+                           name: String,
+                           coordinatesX: BigDecimal,
+                           coordinatesY: BigDecimal
+                          ){
+}
