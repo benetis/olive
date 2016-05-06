85349ab428075ab8737f8d59931d031b23d1679b
zygimantas <zygimantasdev@gmail.com>
2016-05-06 19:33:52 +0300

    schema generator

---
+++ app/models/daos/CreateSchema.scala
@@ -0,0 +1,23 @@
+package models.daos
+
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.LoginInfo
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import slick.lifted.ProvenShape.proveShapeOf
+
+import scala.concurrent.Await
+//TODO needs injection
+class CreateSchema @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends DBTableDefinitions {
+  val driver = slick.driver.MySQLDriver
+  import driver.api._
+
+  lazy val allTables = Array(
+    TableQuery[weather_station].schema
+  ).reduceLeft(_ ++ _)
+
+  def create() = {
+        allTables.create
+  }
+}
