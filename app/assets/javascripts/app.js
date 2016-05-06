b59daa1f2f42836eba56c2a6f854fb06974da032
zygimantas <zygimantasdev@gmail.com>
2016-05-06 19:20:50 +0300

    removed old signupform code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,4 @@
 define(["jquery", "bootstrap"], function($, bootstrap) {
-    var fillSignInForm;
-    fillSignInForm = function($tr) {
-        $('#identifier, #email').val($tr.find('.email').text());
-        return $('#password').val($tr.find('.pwd ').text());
-    };
-    return $(function() {
-        return $('#signin-helper tr').click(function(e) {
-            return fillSignInForm($(this));
-        });
-    });
 });
 
 (function() {
