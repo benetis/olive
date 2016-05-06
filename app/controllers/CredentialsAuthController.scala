e222c24e1879b520db976c57598416bbc91283ef
zygimantas <zygimantasdev@gmail.com>
2016-05-06 12:35:43 +0300

    working on cred error

--- app/controllers/CredentialsAuthController.scala
+++ app/controllers/CredentialsAuthController.scala
@@ -50,6 +50,7 @@ class CredentialsAuthController @Inject() (
    * @return The result to display.
    */
   def authenticate = Action.async { implicit request =>
+
     SignInForm.form.bindFromRequest.fold(
       form => Future.successful(BadRequest(views.html.signIn(form))),
       data => {
