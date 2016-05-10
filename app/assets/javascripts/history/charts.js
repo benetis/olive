951297d6c88a756f7de719794da110693ddd19ca
zygimantas <zygimantasdev@gmail.com>
2016-05-11 02:05:19 +0300

    working AMD with require js

---
+++ app/assets/javascripts/history/charts.js
@@ -0,0 +1,66 @@
+define([ "../main" ], function(main) {
+    require(["jquery"], function($){
+        function buildChart(labels, data) {
+            var ctx = document.getElementById("myChart");
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        fill: false,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        label: 'Temperature °C',
+                        data: data
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                // beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+
+        $.when($.getJSON("/samples/2016.05.10/2016.05.10").then(function(data) {
+            console.log(data);
+            var jsonData = data;
+            arrayClocked = [];
+            arrayRest = [];
+            for(var indexGroup in jsonData){
+                if (!jsonData.hasOwnProperty(indexGroup)) {
+                } else {
+                    // loop in the group in the json data
+                    for (var item in jsonData[indexGroup]) {
+                        var group = jsonData[indexGroup];
+                        if (group.hasOwnProperty(item)) {
+                            if (item == "clocked") {
+                                var date = Date.createFromMysql(group[item]);
+                                arrayClocked.push(date.getHours());
+                            } else {
+                                arrayRest.push(group[item]);
+                            }
+                        }
+                    }
+                }
+            }
+            buildChart(arrayClocked, arrayRest);
+        }));
+
+        Date.createFromMysql = function(mysql_string)
+        {
+            var t, result = null;
+            if( typeof mysql_string === 'string' )
+            {
+                t = mysql_string.split(/[- :]/);
+                //when t[3], t[4] and t[5] are missing they defaults to zero
+                result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+            }
+            return result;
+        };
+    });
+});
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,27 @@
-require.config({
-	paths: {
-		jquery: "../lib/jquery/jquery",
-		bootstrap: "../lib/bootstrap/js/bootstrap"
-	},
-	shim: {
-		jquery: {
-			exports: "$"
-		},
-		bootstrap: {
-			deps: ["jquery"]
-		}
-	}
-});
+(function(requirejs) {
+    "use strict";
+    requirejs.config({
+        baseUrl : "/assets/javascripts",
+        shim : {
+            "jquery" : {
+                exports : "$"
+            },
+            "jsRoutes" : {
+                exports : "jsRoutes"
+            }
+        },
+        paths : {
+            // Map the dependencies to CDNs or WebJars directly
+            "jsRoutes" : "//localhost:9000/jsRoutes.js",
+            "_" : "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
+            "jquery" : "//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js",
+            "bootstrap" : "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"
+            // A WebJars URL would look like
+            // //server:port/webjars/angularjs/1.0.7/angular.min
+        }
+    });
 
-require(['app']);
+    requirejs.onError = function(err) {
+        console.log(err);
+    };
+})(requirejs);
