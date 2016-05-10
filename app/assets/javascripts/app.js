39568ef269c4681ceb14fd879c10c45a0260b55d
zygimantas <zygimantasdev@gmail.com>
2016-05-10 22:44:48 +0300

    temperature graphic reading by hours

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,28 +1,31 @@
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            fill: false,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3]
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+function buildChart(labels, data) {
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                fill: false,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                label: 'Temperature °C',
+                data: data
             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        // beginAtZero:true
+                    }
+                }]
+            }
         }
-    }
-});
+    });
+}
 
-$.getJSON("/samples/2016.05.10/2016.05.10", function(data) {
+$.when($.getJSON("/samples/2016.05.10/2016.05.10").then(function(data) {
+    console.log(data);
     var jsonData = data;
     arrayClocked = [];
     arrayRest = [];
@@ -34,7 +37,8 @@ $.getJSON("/samples/2016.05.10/2016.05.10", function(data) {
                 var group = jsonData[indexGroup];
                 if (group.hasOwnProperty(item)) {
                     if (item == "clocked") {
-                        arrayClocked.push(group[item]);
+                        var date = Date.createFromMysql(group[item]);
+                        arrayClocked.push(date.getHours());
                     } else {
                         arrayRest.push(group[item]);
                     }
@@ -42,5 +46,17 @@ $.getJSON("/samples/2016.05.10/2016.05.10", function(data) {
             }
         }
     }
-    console.log(arrayClocked, arrayRest)
-});
+    buildChart(arrayClocked, arrayRest);
+}));
+
+Date.createFromMysql = function(mysql_string)
+{
+    var t, result = null;
+    if( typeof mysql_string === 'string' )
+    {
+        t = mysql_string.split(/[- :]/);
+        //when t[3], t[4] and t[5] are missing they defaults to zero
+        result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+    }
+    return result;
+};
