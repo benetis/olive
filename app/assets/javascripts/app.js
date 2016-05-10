78f5c1e6287ac4dba0c3ada8c670142a748b0d3d
zygimantas <zygimantasdev@gmail.com>
2016-05-10 14:35:20 +0300

    example chart of votes line chart

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,12 @@
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
+            fill: false,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3]
         }]
