850f4b5ed71b24ac368753a6307cc58ae0bf132e
zygimantas <zygimantasdev@gmail.com>
2016-05-16 18:27:55 +0300

    model opens on fab click + closes on escape key

--- app/assets/javascripts/plantDiseaseModels/plantDiseaseModels.js
+++ app/assets/javascripts/plantDiseaseModels/plantDiseaseModels.js
@@ -1,1 +1,9 @@
-console.log("plant js!");
+define([ "../main" ], function(main) {
+    require(["jquery"], function($) {
+        $(".create-plant-disease").click(function(e) {
+            e.preventDefault();
+            $(".modal").modal("show");
+        });
+
+    });
+});
