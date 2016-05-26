6ae104a19d8d40bb1843e273dc45963936b4c9b1
zygimantas <zygimantasdev@gmail.com>
2016-05-26 11:26:10 +0300

    removed select2

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,7 +15,7 @@
             "jsRoutes" : "//localhost:9000/jsRoutes.js",
             "_" : "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
             "jquery" : "//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js",
-            "bootstrap" : "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"
+            "bootstrap" : "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js",
             // A WebJars URL would look like
             // //server:port/webjars/angularjs/1.0.7/angular.min
         }
