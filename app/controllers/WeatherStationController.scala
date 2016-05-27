0c6cd07f820ee99598aba3097666fc1ab7b1e4f8
zygimantas <zygimantasdev@gmail.com>
2016-05-27 03:34:24 +0300

    weather station visible and accesible only for admin

--- app/controllers/WeatherStationController.scala
+++ app/controllers/WeatherStationController.scala
@@ -3,15 +3,12 @@ package controllers
 import javax.inject._
 
 import com.mohiva.play.silhouette.api.Silhouette
-import com.mohiva.play.silhouette.api.actions.SecuredRequest
-import models.User
 import models.daos.WeatherStationDAO
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
-import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import utils.WithService
 import utils.auth.{AuthenticationController, DefaultEnv}
-
-import scala.concurrent.Future
 
 @Singleton
 class WeatherStationController @Inject()(
@@ -20,7 +17,7 @@ class WeatherStationController @Inject()(
   silhouette: Silhouette[DefaultEnv]
 ) extends Controller with I18nSupport with AuthenticationController {
 
-  def index = silhouette.SecuredAction.async { implicit request =>
+  def index = silhouette.SecuredAction(WithService()).async { implicit request =>
     weatherStationDao.all().map {
       weatherStations => Ok(views.html.weatherStations(weatherStations)) }
   }
